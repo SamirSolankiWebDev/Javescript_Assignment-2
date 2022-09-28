645f84fd5643240a7cf1d6a51f90276cd932b8d5
samirsolanki <W0785689@myscc.ca>
2022-09-28 11:38:10 -0400

    Commit
    Added the code in the file

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// WEB303 Assignment 2
+
+
+function makeHttpRequest(param) {
+  //console.log(param);
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = processResponse;
+  req.open("GET", `./${param}.html`);
+  req.send();
+
+  function processResponse() {
+    if (req.readyState != 4) return; 
+    document.getElementById("content").innerHTML = req.responseText;
+  }
+}
+
+// $("#prospect, #convert, #retain").click(function () {
+//   $("#content").animate({top: '0px'}).empty();
+//   var id = $(this).attr('id');
+//   //console.log(id);
+//   makeHttpRequest(id);
+//   $("#content").animate({top: '100px'}).fadeIn(2500);
+// });
+
+$("#prospect").click(function () {
+  $("#content").animate({top: '0px'}).empty();
+  var id = $(this).attr('id');
+  //console.log(id);
+  makeHttpRequest(id);
+  $("#content").animate({top: '100px'}).fadeIn(2500);
+});
+
+$("#convert").click(function () {
+  $("#content").animate({top: '0px'}).empty();
+  var id = $(this).attr('id');
+  //console.log(id);
+  makeHttpRequest(id);
+  $("#content").animate({top: '100px'}).fadeIn(2500);
+});
+$("#retain").click(function () {
+  $("#content").animate({top: '0px'}).empty();
+  var id = $(this).attr('id');
+  //console.log(id);
+  makeHttpRequest(id);
+  $("#content").animate({top: '100px'}).fadeIn(2500);
+});
